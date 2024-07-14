60d2d09d586baff337ef2b2267dfd57fae3dba3a
Anthony Gbobo <psalmseins@gmail.com>
2024-07-13 21:06:08 -0700

    Create a 'user' table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,11 @@
+-- Using the attribubes, create a user table
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- If table exists, script will not fail, can be executed on any database
+
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR(255)
+);
