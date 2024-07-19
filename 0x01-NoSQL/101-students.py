b849a0ab73ddda08391258fa81f441ec88583ffc
psalmseins01 <141800705+psalmseins01@users.noreply.github.com>
2024-07-19 17:29:16 +0100

    Create 101-students.py
    Returns students sorted by average score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Script that returns all students sorted by average score
+Prototype: def top_students(mongo_collection):
+"""
+
+
+def top_students(mongo_collection):
+    """
+    Prototype: def top_students(mongo_collection):
+    Returns all students sorted by average score
+    """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+            {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {
+            "$sort":
+            {
+                "averageScore": -1
+            }
+        }
+    ])
